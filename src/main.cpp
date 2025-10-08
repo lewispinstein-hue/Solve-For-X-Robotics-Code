3d39dbf83d7bb304508896ef74cc1f13126d7c48
Lewis Pinstein <lewispinstein@gmail.com>
2025-10-07 22:52:23 -0400

    Adding variables I added some variables for the arcade drive and init the motor groups and controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include "main.h"
 
+pros::MotorGroup left_motors_drivetrain({3, 12, 13});
+pros::MotorGroup right_motors_drivetrain({8, -20, 18});
+pros::Controller main_controller(pros::E_CONTROLLER_MASTER);
+
+// init variables for joystick values
+int LEFT_X_AXIS = main_controller.get_analog(
+    pros::E_CONTROLLER_ANALOG_LEFT_X); // LEFT STICK X AXIS
+int LEFT_Y_AXIS = main_controller.get_analog(
+    pros::E_CONTROLLER_ANALOG_LEFT_Y); // LEFT STICK Y AXIS
+
+int RIGHT_X_AXIS = main_controller.get_analog(
+    pros::E_CONTROLLER_ANALOG_RIGHT_X); // RIGHT STICK X AXIS
+int RIGHT_Y_AXIS = main_controller.get_analog(
+    pros::E_CONTROLLER_ANALOG_RIGHT_Y); // RIGHT STICK Y AXIS
+
 /**
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
+
 void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+  static bool pressed = false;
+  pressed = !pressed;
+  if (pressed) {
+    pros::lcd::set_text(2, "I was pressed!");
+  } else {
+    pros::lcd::clear_line(2);
+  }
 }
 
 /**
@@ -23,10 +39,10 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+  pros::lcd::initialize();
+  pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+  pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -74,21 +90,28 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+  while (true) {
+    pros::lcd::print(0, "%d %d %d",
+                     (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+                     (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+                     (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >>
+                         0); // Prints status of the emulated screen LCDs
 
+    // Arcade control scheme
+	
+    int dir = main_controller.get_analog(ANALOG_RIGHT_Y); // Gets amount forward/backward from right joystick
+    int turn = main_controller.get_analog(ANALOG_LEFT_X); // Gets the turn left/right from left joystick
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+    left_motors_drivetrain.move(dir - turn);  // Sets left motor voltage
+    right_motors_drivetrain.move(dir + turn); // Sets right motor voltage
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+    pros::delay(20);                          // Run for 20 ms then update
+  }
+}
+
+int forward = -main_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+int turn = main_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+void arcade_drive(const int forward, const int turn) {
+  left_motors_drivetrain.move(forward + turn);
+  right_motors_drivetrain.move(forward - turn);
 }
